90eaf19bd7254336a3d12905d24bf986ee63d8b2
Oendrila Chakraborty <oendrila@itobuz.com>
2025-01-24 15:49:47 +0530

    FEAT:User data validation

---
+++ backend/src/middleware/user-details-verifier.js
@@ -0,0 +1,26 @@
+import yup from 'yup'
+
+export const userSchema = yup.object({
+
+    userName: yup.string().trim()
+        .min(5, 'username must be at least 8 characters')
+        .max(20,'userName must be at most 20 characters'),
+    email: yup.string()
+        .email('The email is not a valid one').required(),
+    password: yup.string()
+        .min(8, 'Password must be at least 8 characters')
+        .matches(/[A-Z]/, 'Must include at least one uppercase letter')
+        .matches(/[a-z]/, 'Must include at least one lowercase letter')
+        .matches(/\d/, 'Must include at least one number')
+        .matches(/\W/,'Must include at least one special character')
+});
+
+
+export const validateUser = (schema) => async (req, res, next) => {
+    try {
+        await schema.validate(req.body);
+        next();
+    } catch (err) {
+        return res.status(400).json({ errors: err.errors });
+    }
+};
--- backend/src/routes/user-route.js
+++ backend/src/routes/user-route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { register,login} from '../controller/user-controller.js';
 import { verification } from '../middleware/token-verifier.js';
+import { validateUser,userSchema } from '../middleware/user-details-verifier.js';
 
 const route = express.Router();
 
-route.post('/register', register);
+route.post('/register', validateUser(userSchema),register);
 route.get('/verify/:token',verification);
 route.post('/login' , login)
 
